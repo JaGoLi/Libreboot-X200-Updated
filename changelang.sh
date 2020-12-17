6207eb72fce4ff0a2ef32ac50e6c2a7a446a21c7
Jason Goulet-Lipman <jason.gouletlipman@gmail.com>
2020-12-17 15:36:49 -0500

    added language changing utility

---
+++ changelang.sh
@@ -0,0 +1,16 @@
+#! /bin/env bash
+## Copy grub config
+cp libreboot_x200/coreboot-4.13/grub.cfg .
+
+## Modify grub config
+sed -i "34s/usqwerty/${2}/" 'grub.cfg'
+
+## Build cbfstool
+cd libreboot_x200/coreboot-4.13/util/cbfstool
+sudo make install
+cd ../../../..
+
+## Add new grub configuration to rom
+cbfstool ${1} remove -n grub.cfg
+cbfstool ${1} add -n grub.cfg -f grub.cfg -t raw
+mv ${1} ${2}_${1}
