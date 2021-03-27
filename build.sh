94a636c9c000f584c6c5c9b94aff0def98b4ff05
JaGoLi <65408328+JaGoLi@users.noreply.github.com>
2021-03-27 09:41:59 -0400

    Changed script to update version

--- build.sh
+++ build.sh
@@ -3,12 +3,12 @@
 mkdir libreboot_x200 
 cd libreboot_x200
 
-## Get coreboot and extract
-wget https://coreboot.org/releases/coreboot-4.13.tar.xz
-wget https://coreboot.org/releases/coreboot-blobs-4.13.tar.xz
-
-tar -xvf coreboot-4.13.tar.xz
-tar -xvf coreboot-blobs-4.13.tar.xz
+## Get coreboot and match revision
+git clone https://review.coreboot.org/coreboot
+cd coreboot
+git checkout 927fa6d04c
+git submodule update --init --checkout
+cd ..
 
 ## Get libreboot
 git clone https://notabug.org/libreboot/libreboot.git
@@ -29,19 +29,20 @@ sudo make install
 ##Generate grub elf
 cd ../resources/utilities/grub-assemble
 ./gen.sh vesafb
-cp grub_vesafb.elf ../../../../coreboot-4.13/payload.elf
+cp grub_vesafb.elf ../../../../coreboot/payload.elf
 
 ##Get grub background and config
-cd ../../../../coreboot-4.13
+cd ../../../../coreboot
 wget https://github.com/JaGoLi/libreboot-resources/archive/libreboot_res2.tar.gz
 tar -xvf libreboot_res2.tar.gz
 mv libreboot-resources-libreboot_res2/* .
 
 ##Get coreboot configuration
-wget https://github.com/JaGoLi/libreboot-x200-configs/archive/release1.tar.gz
-tar -xvf release1.tar.gz
-cp libreboot-x200-configs-release1/config_${1}_${2} .config
-cp 'libreboot-x200-configs-release1/gbe-ich9m.set' util/bincfg
+wget https://github.com/JaGoLi/libreboot-x200-configs/archive/release2.tar.gz
+tar -xvf release2.tar.gz
+cp libreboot-x200-configs-release2/config_${1}_${2} .config
+cp libreboot-x200-configs-release2/nvram_settings.cfg .
+cp 'libreboot-x200-configs-release2/gbe-ich9m.set' util/bincfg
 
 ##Compile utilities
 cd util/cbfstool
@@ -76,11 +77,7 @@ cbfstool x200.rom add -f grub.cfg -n grub.cfg -t raw
 cbfstool x200.rom add -f background.jpg -n background.jpg -t raw
 
 ## Change nvram settings
-nvramtool -C x200.rom -w debug_level=Spew
-nvramtool -C x200.rom -w bluetooth=Disable
-nvramtool -C x200.rom -w power_management_beeps=Disable
-nvramtool -C x200.rom -w low_battery_beep=Disable
-nvramtool -C x200.rom -w gfx_uma_size=352M
+nvramtool -C x200.rom -p nvram_settings.cfg
 
 ## Export rom to home directory
 mkdir ../../roms
